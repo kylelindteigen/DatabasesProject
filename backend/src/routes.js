56a1ab153a1b45088cf8ee4e9c02c17be25b69c9
kylelindteigen <kylelindteigen@ku.edu>
2020-04-06 12:46:01 -0500

    added some queries

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,6 +4,8 @@ const sql = require('./sql.js')
 
 sql.init();
 sql.login('kylelindteigen', 'password');
+sql.getcustposts('0')
+sql.getresttposts('0')
 var Router = express.Router();
 
 
--- backend/src/sql.js
+++ backend/src/sql.js
@@ -24,8 +24,39 @@ class SQL{
 		})
 	}
 
-	static getfollows(customerid){
+	static getcustposts(customerid){
+		return db.query("SELECT P.CustomerID, P.PostID, Name, TimeStamp, Post FROM (SELECT P.PostID, Customer.CustomerID, Name, TimeStamp, Post FROM (SELECT Post.PostID, CustomerID, TimeStamp, Post FROM CustPost LEFT OUTER JOIN Post ON Post.PostID = CustPost.PostID) AS P LEFT OUTER JOIN Customer ON P.CustomerID = Customer.CustomerID) AS P, (SELECT CustomerID2 AS CustomerID FROM CustFollowsCust WHERE CustomerID1 = " + customerid + ") AS F WHERE F.CustomerID = P.CustomerID", function (err, result, fields) {
+			if(err) throw err;
+			console.log(result);
+		})
+	}
 
+	static getrestposts(customerid){
+		return db.query("SELECT P.RestaurantID, P.PostID, Name, TimeStamp, Post FROM (SELECT P.PostID, Restaurant.RestaurantID, Name, TimeStamp, Post FROM (SELECT Post.PostID, RestaurantID, TimeStamp, Post FROM RestPost LEFT OUTER JOIN Post ON Post.PostID = RestPost.PostID) AS P LEFT OUTER JOIN Restaurant ON P.RestaurantID = Restaurant.RestaurantID) AS P, (SELECT RestaurantID FROM CustFollowsRest WHERE CustomerID = "+ customerid +") AS F WHERE F.RestaurantID = P.RestaurantID", function (err, result, fields) {
+			if(err) throw err;
+			console.log(result);
+		})
+	}
+
+	static getuserposts(customerid){
+		return db.query("SELECT P.PostID, Name, TimeStamp, Post FROM (SELECT P.PostID, Customer.CustomerID, Name, TimeStamp, Post FROM (SELECT Post.PostID, CustomerID, TimeStamp, Post FROM CustPost LEFT OUTER JOIN Post ON Post.PostID = CustPost.PostID) AS P LEFT OUTER JOIN Customer ON P.CustomerID = Customer.CustomerID) AS P WHERE P.CustomerID = " + customerid, function (err, result, fields) {
+			if(err) throw err;
+			console.log(result);
+		})
+	}
+
+	static getrestposts(customerid){
+		return db.query("SELECT P.RestaurantID, P.PostID, Name, TimeStamp, Post FROM (SELECT P.PostID, Restaurant.RestaurantID, Name, TimeStamp, Post FROM (SELECT Post.PostID, RestaurantID, TimeStamp, Post FROM RestPost LEFT OUTER JOIN Post ON Post.PostID = RestPost.PostID) AS P LEFT OUTER JOIN Restaurant ON P.RestaurantID = Restaurant.RestaurantID) AS P WHERE P.RestaurantID = " + customerid, function (err, result, fields) {
+			if(err) throw err;
+			console.log(result);
+		})
+	}
+
+	static getrestreview(restaurantid){
+		return db.query("SELECT P.PostID, Name, TimeStamp, Post FROM (SELECT P.PostID, Customer.CustomerID, Name, TimeStamp, Post FROM (SELECT Post.PostID, CustomerID, TimeStamp, Post FROM CustPost LEFT OUTER JOIN Post ON Post.PostID = CustPost.PostID) AS P LEFT OUTER JOIN Customer ON P.CustomerID = Customer.CustomerID) AS P WHERE P.CustomerID = " + customerid, function (err, result, fields) {
+			if(err) throw err;
+			console.log(result);
+		})
 	}
 	//static registerUser(username, password,
 }
